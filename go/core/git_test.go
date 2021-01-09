78bb45e85e77f3dfbbe497fd439acd3790827374
mp <>
2021-01-09 22:39:53 +0100

    Added unit test
    ============

--- go/core/git_test.go
+++ go/core/git_test.go
@@ -14,3 +14,23 @@ func TestStatus(t *testing.T) {
 	gitStatus, err := GetGitStatus(project)
 	t.Logf("Gitfiles %v", gitStatus)
 }
+
+
+func TestCommit(t *testing.T) {
+	project, err := OpenProject("../../.ash")
+	assert.NotNilf(t, err, "Cannot open project: %w", err)
+
+	commitInfo := CommitInfo{
+		User:     "mp",
+		Header:   "This is just a test",
+		Comments: map[string]string{
+			"task 1": "a comment",
+			"task 2": "another comment",
+			"task 3": "final comment",
+		},
+		Files:    []string{},
+	}
+
+	hash, err := GitCommit(project, commitInfo)
+	t.Logf("GitCommit %v", hash)
+}
